7df06eb05f22a09d4e4b9db5c516db340183798d
temifowotade <temitope.fowotade@andela.com>
2016-08-04 17:14:24 +0100

    deletes files for tdd

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      root "bucket_lists#welcome"
-
-      resources :bucketlists, controller: :bucket_lists, except: [:new, :edit] do
-        resources :items, except: [:new, :edit]
-      end
       post "/auth/login" => "authentication#login"
       get "/auth/logout" => "authentication#logout"
     end
