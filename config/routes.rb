e3f8ce41f946c8bf57860cc8091f566b70ddc755
temifowotade <temitope.fowotade@andela.com>
2016-08-04 17:19:12 +0100

    removes authentication from testing app for authentication branch for tdd implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post "/auth/login" => "authentication#login"
-      get "/auth/logout" => "authentication#logout"
+      
     end
   end
 end
