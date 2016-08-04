12182ebdfdcf761ef6dbdaa9d64335b3af6214de
temifowotade <temitope.fowotade@andela.com>
2016-08-04 17:26:35 +0100

    test: adds tests for user authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  protected
+
+  def authenticate_request!
+    unless user_id_in_token?
+      render json: { error: "Not Authenticated" }, status: :unauthorized
+      return
+    end
+    @current_user = User.find(auth_token[:user_id])
+
+    unless @current_user.token
+      render json: { error: "You're logged out! Please login to continue." },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def http_token
+    @http_token ||= if request.headers["Authorization"].present?
+                      request.headers["Authorization"].split(" ").last
+    end
+  end
+
+  def auth_token
+    @auth_token ||= JsonWebToken.decode(http_token)
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      
+      post "/auth/login" => "authentication#login"
+      get "/auth/logout" => "authentication#logout"
     end
   end
 end
