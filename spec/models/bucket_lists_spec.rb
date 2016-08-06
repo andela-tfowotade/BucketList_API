fda0e83315baacd52e4e44707a239ec4f61dd49c
temifowotade <temitope.fowotade@andela.com>
2016-08-06 17:14:14 +0100

    test: adds tests for search

--- spec/models/bucket_lists_spec.rb
+++ spec/models/bucket_lists_spec.rb
@@ -30,4 +30,38 @@ describe BucketList, type: :model do
       end
     end
   end
+
+  describe ".search" do
+    context "when query matches a bucket list" do
+      it "returns the filtered list using the query supplied" do
+        john = create(:bucket_list, name: "Cities I'll travel to")
+        peter = create(:bucket_list, name: "Career goals")
+
+        expect(BucketList.search({q: "travel"})).to eq([john])
+      end
+    end
+
+    context "when query doesn't match a bucket list" do
+      it "returns an empty array" do
+        expect(BucketList.search({q: "travel"})).to eq([])
+      end
+    end
+  end
+
+  describe ".paginate_and_search" do
+    context "with empty bucket list" do 
+      it "returns an empty array" do
+        expect(BucketList.paginate_and_search({q: "bucket", page: 1, limit: 2})).to eq([])
+      end
+    end
+
+    context "with bucket lists present" do
+      it "returns the paginated and query filtered list" do
+        john = create(:bucket_list, name: "Cities I'll travel to")
+        peter = create(:bucket_list, name: "Career goals")
+
+        expect(BucketList.paginate_and_search({q: "travel", page: 1, limit: 2})).to eq([john])
+      end
+    end
+  end
 end
