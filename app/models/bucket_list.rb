a1f2896243ff5c80c9a96d3be1aa53425b8d8440
temifowotade <temitope.fowotade@andela.com>
2016-08-06 17:12:11 +0100

    feat(pagination): adds pagination to BucketList. [finishes #127775791]

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -3,4 +3,8 @@ class BucketList < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true
   validates :created_by, presence: true
-end
+
+  scope :paginate, lambda { |page, page_limit| 
+    limit(page_limit).offset(page_limit.to_i * ([page.to_i, 1].max - 1)) 
+  }
+end 
