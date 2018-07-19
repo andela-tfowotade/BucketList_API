54bb794248293f60b5e3404333a7530e451cdc9b
andela-tfowotade <temitope.fowotade@andela.com>
2018-07-19 11:40:29 +0100

    refactor routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,27 @@
 Rails.application.routes.draw do
+  def shared_routes
+    resources :bucketlists, controller: :bucket_lists, except: [:new, :edit] do
+      resources :items, except: [:new, :edit]
+    end
+
+    scope "/auth", controller: :authentication do
+      post "/create_user" => :create
+      post "/login" => :login
+      get "/logout" => :logout
+    end
+
+    get "/" => "authentication#welcome"
+  end
+
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
-      resources :bucketlists, controller: :bucket_lists, except: [:new, :edit] do
-        resources :items, except: [:new, :edit]
-      end
-
-      scope "/auth", controller: :authentication do
-        post "/create_user" => :create
-        post "/login" => :login
-        get "/logout" => :logout
-      end
-
-      get "/" => "authentication#welcome"
+      shared_routes
     end
   end
 
   namespace :api, defaults: { format: "json" } do
     namespace :v2 do
-      resources :bucketlists, controller: :bucket_lists, except: [:new, :edit] do
-        resources :items, except: [:new, :edit]
-      end
-
-      scope "/auth", controller: :authentication do
-        post "/create_user" => :create
-        post "/login" => :login
-        get "/logout" => :logout
-      end
-
-      get "/" => "authentication#welcome"
+      shared_routes
     end
   end
 
